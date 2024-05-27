61d2abaaecf7c09521028bee08af8f1e27bd06b7
Yunkon Kim <hermitkim1@gmail.com>
2024-05-27 19:35:58 +0900

    Hide the unused APIs

--- pkg/api/rest/common/config.go
+++ pkg/api/rest/common/config.go
@@ -14,146 +14,137 @@ limitations under the License.
 // Package common is to handle REST API for common funcitonalities
 package common
 
-import (
-	"fmt"
-	"net/http"
+// // RestInitConfig godoc
+// // @Summary Init config
+// // @Description Init config
+// // @Tags [Admin] System environment
+// // @Accept  json
+// // @Produce  json
+// // @Param configId path string true "Config ID"
+// // @Success 200 {object} common.ConfigInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /config/{configId} [delete]
+// func RestInitConfig(c echo.Context) error {
+// 	//id := c.Param("configId")
+// 	if err := Validate(c, []string{"configId"}); err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	"github.com/labstack/echo/v4"
+// 	err := common.InitConfig(c.Param("configId"))
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusOK, "Failed to init the config "+c.Param("configId"))
+// 	} else {
+// 		return SendMessage(c, http.StatusOK, "The config "+c.Param("configId")+" has been initialized.")
+// 	}
+// }
 
-	"github.com/cloud-barista/cm-beetle/pkg/core/common"
-)
+// // RestGetConfig godoc
+// // @Summary Get config
+// // @Description Get config
+// // @Tags [Admin] System environment
+// // @Accept  json
+// // @Produce  json
+// // @Param configId path string true "Config ID"
+// // @Success 200 {object} common.ConfigInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /config/{configId} [get]
+// func RestGetConfig(c echo.Context) error {
+// 	//id := c.Param("configId")
+// 	if err := Validate(c, []string{"configId"}); err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-// RestInitConfig godoc
-// @Summary Init config
-// @Description Init config
-// @Tags [Admin] System environment
-// @Accept  json
-// @Produce  json
-// @Param configId path string true "Config ID"
-// @Success 200 {object} common.ConfigInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /config/{configId} [delete]
-func RestInitConfig(c echo.Context) error {
-	//id := c.Param("configId")
-	if err := Validate(c, []string{"configId"}); err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// 	res, err := common.GetConfig(c.Param("configId"))
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusOK, "Failed to find the config "+c.Param("configId"))
+// 	} else {
+// 		return Send(c, http.StatusOK, res)
+// 	}
+// }
 
-	err := common.InitConfig(c.Param("configId"))
-	if err != nil {
-		return SendMessage(c, http.StatusOK, "Failed to init the config "+c.Param("configId"))
-	} else {
-		return SendMessage(c, http.StatusOK, "The config "+c.Param("configId")+" has been initialized.")
-	}
-}
+// // Response structure for RestGetAllConfig
+// type RestGetAllConfigResponse struct {
+// 	//Name string     `json:"name"`
+// 	Config []common.ConfigInfo `json:"config"`
+// }
 
-// RestGetConfig godoc
-// @Summary Get config
-// @Description Get config
-// @Tags [Admin] System environment
-// @Accept  json
-// @Produce  json
-// @Param configId path string true "Config ID"
-// @Success 200 {object} common.ConfigInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /config/{configId} [get]
-func RestGetConfig(c echo.Context) error {
-	//id := c.Param("configId")
-	if err := Validate(c, []string{"configId"}); err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// // RestGetAllConfig godoc
+// // @Summary List all configs
+// // @Description List all configs
+// // @Tags [Admin] System environment
+// // @Accept  json
+// // @Produce  json
+// // @Success 200 {object} RestGetAllConfigResponse
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /config [get]
+// func RestGetAllConfig(c echo.Context) error {
 
-	res, err := common.GetConfig(c.Param("configId"))
-	if err != nil {
-		return SendMessage(c, http.StatusOK, "Failed to find the config "+c.Param("configId"))
-	} else {
-		return Send(c, http.StatusOK, res)
-	}
-}
+// 	var content RestGetAllConfigResponse
 
-// Response structure for RestGetAllConfig
-type RestGetAllConfigResponse struct {
-	//Name string     `json:"name"`
-	Config []common.ConfigInfo `json:"config"`
-}
+// 	configList, err := common.ListConfig()
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusOK, "Failed to list configs.")
+// 	}
 
-// RestGetAllConfig godoc
-// @Summary List all configs
-// @Description List all configs
-// @Tags [Admin] System environment
-// @Accept  json
-// @Produce  json
-// @Success 200 {object} RestGetAllConfigResponse
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /config [get]
-func RestGetAllConfig(c echo.Context) error {
+// 	if configList == nil {
+// 		return Send(c, http.StatusOK, content)
+// 	}
 
-	var content RestGetAllConfigResponse
+// 	// When err == nil && resourceList != nil
+// 	content.Config = configList
+// 	return Send(c, http.StatusOK, content)
 
-	configList, err := common.ListConfig()
-	if err != nil {
-		return SendMessage(c, http.StatusOK, "Failed to list configs.")
-	}
+// }
 
-	if configList == nil {
-		return Send(c, http.StatusOK, content)
-	}
+// // RestPostConfig godoc
+// // @Summary Create or Update config
+// // @Description Create or Update config (SPIDER_REST_URL, DRAGONFLY_REST_URL, ...)
+// // @Tags [Admin] System environment
+// // @Accept  json
+// // @Produce  json
+// // @Param config body common.ConfigReq true "Key and Value for configuration"
+// // @Success 200 {object} common.ConfigInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /config [post]
+// func RestPostConfig(c echo.Context) error {
 
-	// When err == nil && resourceList != nil
-	content.Config = configList
-	return Send(c, http.StatusOK, content)
+// 	u := &common.ConfigReq{}
+// 	if err := c.Bind(u); err != nil {
+// 		//return err
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-}
+// 	fmt.Println("[Creating or Updating Config]")
+// 	content, err := common.UpdateConfig(u)
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
+// 	return Send(c, http.StatusOK, content)
 
-// RestPostConfig godoc
-// @Summary Create or Update config
-// @Description Create or Update config (SPIDER_REST_URL, DRAGONFLY_REST_URL, ...)
-// @Tags [Admin] System environment
-// @Accept  json
-// @Produce  json
-// @Param config body common.ConfigReq true "Key and Value for configuration"
-// @Success 200 {object} common.ConfigInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /config [post]
-func RestPostConfig(c echo.Context) error {
+// }
 
-	u := &common.ConfigReq{}
-	if err := c.Bind(u); err != nil {
-		//return err
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// // RestInitAllConfig godoc
+// // @Summary Init all configs
+// // @Description Init all configs
+// // @Tags [Admin] System environment
+// // @Accept  json
+// // @Produce  json
+// // @Success 200 {object} common.SimpleMsg
+// // @Failure 404 {object} common.SimpleMsg
+// // @Router /config [delete]
+// func RestInitAllConfig(c echo.Context) error {
 
-	fmt.Println("[Creating or Updating Config]")
-	content, err := common.UpdateConfig(u)
-	if err != nil {
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
-	return Send(c, http.StatusOK, content)
+// 	err := common.InitAllConfig()
+// 	if err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-}
-
-// RestInitAllConfig godoc
-// @Summary Init all configs
-// @Description Init all configs
-// @Tags [Admin] System environment
-// @Accept  json
-// @Produce  json
-// @Success 200 {object} common.SimpleMsg
-// @Failure 404 {object} common.SimpleMsg
-// @Router /config [delete]
-func RestInitAllConfig(c echo.Context) error {
-
-	err := common.InitAllConfig()
-	if err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
-
-	return SendMessage(c, http.StatusOK, "All configs has been initialized.")
-}
+// 	return SendMessage(c, http.StatusOK, "All configs has been initialized.")
+// }
--- pkg/api/rest/common/namespace.go
+++ pkg/api/rest/common/namespace.go
@@ -43,181 +43,181 @@ func RestCheckNs(c echo.Context) error {
 	return SendExistence(c, http.StatusOK, exists)
 }
 
-// RestDelAllNs godoc
-// @Summary Delete all namespaces
-// @Description Delete all namespaces
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Success 200 {object} common.SimpleMsg
-// @Failure 404 {object} common.SimpleMsg
-// @Router /ns [delete]
-func RestDelAllNs(c echo.Context) error {
+// // RestDelAllNs godoc
+// // @Summary Delete all namespaces
+// // @Description Delete all namespaces
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Success 200 {object} common.SimpleMsg
+// // @Failure 404 {object} common.SimpleMsg
+// // @Router /ns [delete]
+// func RestDelAllNs(c echo.Context) error {
 
-	err := common.DelAllNs()
-	if err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// 	err := common.DelAllNs()
+// 	if err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	return SendMessage(c, http.StatusOK, "All namespaces has been deleted")
-}
+// 	return SendMessage(c, http.StatusOK, "All namespaces has been deleted")
+// }
 
-// RestDelNs godoc
-// @Summary Delete namespace
-// @Description Delete namespace
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Param nsId path string true "Namespace ID" default(ns01)
-// @Success 200 {object} common.SimpleMsg
-// @Failure 404 {object} common.SimpleMsg
-// @Router /ns/{nsId} [delete]
-func RestDelNs(c echo.Context) error {
+// // RestDelNs godoc
+// // @Summary Delete namespace
+// // @Description Delete namespace
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Param nsId path string true "Namespace ID" default(ns01)
+// // @Success 200 {object} common.SimpleMsg
+// // @Failure 404 {object} common.SimpleMsg
+// // @Router /ns/{nsId} [delete]
+// func RestDelNs(c echo.Context) error {
 
-	if err := Validate(c, []string{"nsId"}); err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// 	if err := Validate(c, []string{"nsId"}); err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	err := common.DelNs(c.Param("nsId"))
-	if err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// 	err := common.DelNs(c.Param("nsId"))
+// 	if err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	return SendMessage(c, http.StatusOK, "The ns "+c.Param("nsId")+" has been deleted")
-}
+// 	return SendMessage(c, http.StatusOK, "The ns "+c.Param("nsId")+" has been deleted")
+// }
 
-// JSONResult's data field will be overridden by the specific type
-type JSONResult struct {
-	//Code    int          `json:"code" `
-	//Message string       `json:"message"`
-	//Data    interface{}  `json:"data"`
-}
+// // JSONResult's data field will be overridden by the specific type
+// type JSONResult struct {
+// 	//Code    int          `json:"code" `
+// 	//Message string       `json:"message"`
+// 	//Data    interface{}  `json:"data"`
+// }
 
-// Response structure for RestGetAllNs
-type RestGetAllNsResponse struct {
-	//Name string     `json:"name"`
-	Ns []common.NsInfo `json:"ns"`
-}
+// // Response structure for RestGetAllNs
+// type RestGetAllNsResponse struct {
+// 	//Name string     `json:"name"`
+// 	Ns []common.NsInfo `json:"ns"`
+// }
 
-// RestGetAllNs godoc
-// @Summary List all namespaces or namespaces' ID
-// @Description List all namespaces or namespaces' ID
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Param option query string false "Option" Enums(id)
-// @Success 200 {object} JSONResult{[DEFAULT]=RestGetAllNsResponse,[ID]=common.IdList} "Different return structures by the given option param"
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /ns [get]
-func RestGetAllNs(c echo.Context) error {
+// // RestGetAllNs godoc
+// // @Summary List all namespaces or namespaces' ID
+// // @Description List all namespaces or namespaces' ID
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Param option query string false "Option" Enums(id)
+// // @Success 200 {object} JSONResult{[DEFAULT]=RestGetAllNsResponse,[ID]=common.IdList} "Different return structures by the given option param"
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /ns [get]
+// func RestGetAllNs(c echo.Context) error {
 
-	optionFlag := c.QueryParam("option")
+// 	optionFlag := c.QueryParam("option")
 
-	var content RestGetAllNsResponse
-	if optionFlag == "id" {
-		content := common.IdList{}
+// 	var content RestGetAllNsResponse
+// 	if optionFlag == "id" {
+// 		content := common.IdList{}
 
-		var err error
-		content.IdList, err = common.ListNsId()
-		if err != nil {
-			return SendMessage(c, http.StatusOK, "Failed to list namespaces' ID: "+err.Error())
-		}
+// 		var err error
+// 		content.IdList, err = common.ListNsId()
+// 		if err != nil {
+// 			return SendMessage(c, http.StatusOK, "Failed to list namespaces' ID: "+err.Error())
+// 		}
 
-		return c.JSON(http.StatusOK, &content)
-	} else {
-		nsList, err := common.ListNs()
-		if err != nil {
-			return SendMessage(c, http.StatusOK, "Failed to list namespaces.")
-		}
+// 		return c.JSON(http.StatusOK, &content)
+// 	} else {
+// 		nsList, err := common.ListNs()
+// 		if err != nil {
+// 			return SendMessage(c, http.StatusOK, "Failed to list namespaces.")
+// 		}
 
-		if nsList == nil {
-			return Send(c, http.StatusOK, content)
-		}
+// 		if nsList == nil {
+// 			return Send(c, http.StatusOK, content)
+// 		}
 
-		// When err == nil && resourceList != nil
-		content.Ns = nsList
-		return Send(c, http.StatusOK, content)
-	}
-}
+// 		// When err == nil && resourceList != nil
+// 		content.Ns = nsList
+// 		return Send(c, http.StatusOK, content)
+// 	}
+// }
 
-// RestGetNs godoc
-// @Summary Get namespace
-// @Description Get namespace
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Param nsId path string true "Namespace ID" default(ns01)
-// @Success 200 {object} common.NsInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /ns/{nsId} [get]
-func RestGetNs(c echo.Context) error {
-	//id := c.Param("nsId")
-	if err := Validate(c, []string{"nsId"}); err != nil {
-		common.CBLog.Error(err)
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// // RestGetNs godoc
+// // @Summary Get namespace
+// // @Description Get namespace
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Param nsId path string true "Namespace ID" default(ns01)
+// // @Success 200 {object} common.NsInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /ns/{nsId} [get]
+// func RestGetNs(c echo.Context) error {
+// 	//id := c.Param("nsId")
+// 	if err := Validate(c, []string{"nsId"}); err != nil {
+// 		common.CBLog.Error(err)
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	res, err := common.GetNs(c.Param("nsId"))
-	if err != nil {
-		return SendMessage(c, http.StatusOK, "Failed to find the namespace "+c.Param("nsId"))
-	} else {
-		return Send(c, http.StatusOK, res)
-	}
-}
+// 	res, err := common.GetNs(c.Param("nsId"))
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusOK, "Failed to find the namespace "+c.Param("nsId"))
+// 	} else {
+// 		return Send(c, http.StatusOK, res)
+// 	}
+// }
 
-// RestPostNs godoc
-// @Summary Create namespace
-// @Description Create namespace
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Param nsReq body common.NsReq true "Details for a new namespace"
-// @Success 200 {object} common.NsInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /ns [post]
-func RestPostNs(c echo.Context) error {
+// // RestPostNs godoc
+// // @Summary Create namespace
+// // @Description Create namespace
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Param nsReq body common.NsReq true "Details for a new namespace"
+// // @Success 200 {object} common.NsInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /ns [post]
+// func RestPostNs(c echo.Context) error {
 
-	u := &common.NsReq{}
-	if err := c.Bind(u); err != nil {
-		//return err
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// 	u := &common.NsReq{}
+// 	if err := c.Bind(u); err != nil {
+// 		//return err
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	content, err := common.CreateNs(u)
-	if err != nil {
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
-	return Send(c, http.StatusOK, content)
+// 	content, err := common.CreateNs(u)
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
+// 	return Send(c, http.StatusOK, content)
 
-}
+// }
 
-// RestPutNs godoc
-// @Summary Update namespace
-// @Description Update namespace
-// @Tags [Namespace] Namespace management
-// @Accept  json
-// @Produce  json
-// @Param nsId path string true "Namespace ID" default(ns01)
-// @Param namespace body common.NsReq true "Details to update existing namespace"
-// @Success 200 {object} common.NsInfo
-// @Failure 404 {object} common.SimpleMsg
-// @Failure 500 {object} common.SimpleMsg
-// @Router /ns/{nsId} [put]
-func RestPutNs(c echo.Context) error {
-	u := &common.NsReq{}
-	if err := c.Bind(u); err != nil {
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
+// // RestPutNs godoc
+// // @Summary Update namespace
+// // @Description Update namespace
+// // @Tags [Namespace] Namespace management
+// // @Accept  json
+// // @Produce  json
+// // @Param nsId path string true "Namespace ID" default(ns01)
+// // @Param namespace body common.NsReq true "Details to update existing namespace"
+// // @Success 200 {object} common.NsInfo
+// // @Failure 404 {object} common.SimpleMsg
+// // @Failure 500 {object} common.SimpleMsg
+// // @Router /ns/{nsId} [put]
+// func RestPutNs(c echo.Context) error {
+// 	u := &common.NsReq{}
+// 	if err := c.Bind(u); err != nil {
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
 
-	content, err := common.UpdateNs(c.Param("nsId"), u)
-	if err != nil {
-		return SendMessage(c, http.StatusBadRequest, err.Error())
-	}
-	return Send(c, http.StatusOK, content)
-}
+// 	content, err := common.UpdateNs(c.Param("nsId"), u)
+// 	if err != nil {
+// 		return SendMessage(c, http.StatusBadRequest, err.Error())
+// 	}
+// 	return Send(c, http.StatusOK, content)
+// }
