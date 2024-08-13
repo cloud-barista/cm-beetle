a3e8526d8c5b6245d465c29c1567d92511cb20f3
Yunkon Kim <hermitkim1@gmail.com>
2024-08-13 17:42:24 +0900

    Forward some specific requests to the tumblebug API server
    * Set route groups
    * Note - the route pkg will be deprecated.

--- pkg/api/rest/route/recommendation.go
+++ pkg/api/rest/route/recommendation.go
@@ -1,11 +1,11 @@
 package route
 
-import (
-	"github.com/cloud-barista/cm-beetle/pkg/api/rest/controller"
-	"github.com/labstack/echo/v4"
-)
+// import (
+// 	"github.com/cloud-barista/cm-beetle/pkg/api/rest/controller"
+// 	"github.com/labstack/echo/v4"
+// )
 
-// /beetle/recommendation/*
-func RegisterRecommendationRoutes(g *echo.Group) {
-	g.POST("/infra", controller.RecommendInfra)
-}
+// // /beetle/recommendation/*
+// func RegisterRecommendationRoutes(g *echo.Group) {
+// 	g.POST("/infra", controller.RecommendInfra)
+// }
